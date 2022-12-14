238d601eed5420fcaa271817609d6d66b4f632ce
DanielSamuels <35511897+danielfishbone@users.noreply.github.com>
2022-12-14 23:53:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+total = input("What was the total bill? ")
+percentage = input("what percentage tip would you like to give? 10,12 or 15? ")
+people = input("How many people to split the bill? ")
+_total = float(total)
+_percentage = _total * (float(percentage) / 100)
+_people = int(people)
+pay_amount = (_total + _percentage) / _people
+pay_amount = "{:.2f}".format(pay_amount)
+print(f"Each person should pay: ${pay_amount}")
